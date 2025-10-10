11294699a1f78632b559b78b8361a4cfe93866c8
Fernando Pérez <fpl81660@lasallebajio.edu.mx>
2025-10-10 16:30:06 -0600

    Primer commit del proyecto

---
+++ index.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const app = express();
+const port =3000;
+
+app.use(express.json());
+
+app.get("/",(req,res)=>
+{
+    res.send("Hola desde mi server de express 3");
+});
+
+app.get("/nuevaruta",(req,res)=>
+{
+    res.send("Hola este es el segundo end point");
+});
+
+
+app.get("categories/:categoryId/products/:productId", (req,res) =>{
+    const {categoryId, productId}= req.params;
+    res.json({
+        categoryId,
+        productId
+    })
+})
+
+app.get('/users',(req,res)=>{
+    const{username, lastname} = req.query;
+    if ( username && lastname)
+    {
+        res.json({
+            username,
+            lastname
+        });
+    }
+    else{
+        res.send("No hay parametros de ruta")
+    }
+})
+
+app.listen(port, ()=>
+{
+    console.log("My port is working on: "+port);
+})
+
+
+
+
+
+
+/*const express = require('express');
+const routerApi = require('./routes/rutas');
+const router = require('./routes/productsRouters');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my Express app!');
+});
+
+app.get('/nuevaruta', (req, res) => {
+  res.send('This is a new route!');
+});
+
+/* Dar de alta */
+
+
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.json({
+    categoryId,
+    productId
+  });
+});
+
+app.listen(port, () => {
+  console.log("My port us working on: " + port);
+});
+
+
+/*
+
+api.example.com/task/{id}
+
+api.example.com/people/{id}
+
+aoi.example.com/users/{id}/tasks/{id}
+*/
